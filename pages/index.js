df9f3160b197c74a6bd7f6c8873d899799a48f6e
theoldtree <yjeongmu@gmail.com>
2023-07-18 16:28:52 +0900

    nextjs 튜토리얼 moduleCSS globalCSS OpenGraph-meta_property_content

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi My Name is JM. I'm Making Website. I'm familiar with Front End,
+          Back End, and IOT
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
